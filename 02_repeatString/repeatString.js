848886269eea5d2a17031f78ad9210070e058fd6
faun3 <antonioculcea@gmail.com>
2022-12-15 21:55:36 +0200

    change files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) 
+{
+    let result = "";
+    if (num >= 0)
+    {
+        if (num != 0)
+        {
+            let copy = string;
+            while (num)
+            {
+                result += string;
+                num--;
+            }
+            return result;
+        }
+        else
+        {
+            return result;
+        }
+    }
+    else return "ERROR";
 };
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
