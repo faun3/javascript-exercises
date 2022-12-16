f836e78b7fd86f828a6a3a9f96ac9a16a9c3d609
faun3 <antonioculcea@gmail.com>
2022-12-16 21:50:05 +0200

    finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    for (let i = string.length; i > 0; i--)
+    {
+        let temp = string.substring(i - 1, i);
+        result += temp;
+    }
+    return result;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
