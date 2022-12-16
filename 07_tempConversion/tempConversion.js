e1d27c1bf87b03bca09fd591d6cd1c7f5ee252b3
faun3 <antonioculcea@gmail.com>
2022-12-16 22:39:41 +0200

    finish exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  farenheitTemp -= 32;
+  farenheitTemp *= 5;
+  farenheitTemp /= 9;
+  farenheitTemp = parseFloat(farenheitTemp.toFixed(1));
+  return farenheitTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  celsiusTemp *= 9;
+  celsiusTemp /= 5;
+  celsiusTemp += 32;
+  celsiusTemp = parseFloat(celsiusTemp.toFixed(1));
+  return celsiusTemp;
 };
 
 // Do not edit below this line
