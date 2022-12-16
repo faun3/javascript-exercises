a65453d269de41ef381bb2d09d61859bcfd4568d
faun3 <antonioculcea@gmail.com>
2022-12-16 22:13:40 +0200

    finish exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    for (let i = 0; i < array.length; i++)
+    {
+        remove.sort();
+        for (let j = 0; j < remove.length; j++)
+        {
+            if (array[i] === remove[j])
+            {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
