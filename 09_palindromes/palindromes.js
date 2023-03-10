40769b77b3b2b228eb30857dc8c987659cebf274
faun3 <antonioculcea@gmail.com>
2023-03-10 22:39:07 +0200

    pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    const punctuationRegex = /[! `"#$%&'()+-,-./:;<=>?@[\]^_`{|}~]/g;
+    let cleanedWord = word.replace(punctuationRegex, '');
+    let reversedWord = cleanedWord.split('').reverse().join('');
+    if (cleanedWord === reversedWord) return true;
+    else return false;
 };
+
+palindromes("Rac er ! Tr !! aces");
 
 // Do not edit below this line
 module.exports = palindromes;
