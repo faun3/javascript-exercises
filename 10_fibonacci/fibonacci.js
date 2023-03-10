e62fa5ba321237326c85354d9f67ba6510f5c9d4
faun3 <antonioculcea@gmail.com>
2023-03-10 22:45:43 +0200

    pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    let index = Number(pos);
+    let counter = 2;
+    if (!index) return;
+    if (index < 0) return "OOPS";
+    let value1 = 1, value2 = 1;
+    while (counter <= index){
+        if (index === 1 || index === 2) return 1;
+        let temp = value1 + value2;
+        value1 = value2;
+        value2 = temp;
+        counter++;
+    }
+    return value1;
 };
 
 // Do not edit below this line
